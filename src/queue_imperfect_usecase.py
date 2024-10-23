9a8a16216bfbfb34bd2cf5b24e53d79bb6e747e8
MartinFiorde <fiorde_90@hotmail.com>
2024-10-23 19:10:02 -0300

    3-C1 solution

--- src/queue_imperfect_usecase.py
+++ src/queue_imperfect_usecase.py
@@ -1,5 +1,14 @@
-from queue import Queue
+from src.queue_imperfect import QueueMatchmaking
 
 
-def matchmake(queue, user):
-    pass
+def matchmake(queue: QueueMatchmaking, user: tuple[str, str]):
+    name, action = user
+    if action == "leave":
+        queue.search_and_remove(name)
+    elif action == "join":
+        queue.push(name)
+    if queue.size() >= 4:
+        user1 = queue.pop()
+        user2 = queue.pop()
+        return f"{user1} matched {user2}!"
+    return "No match found"
