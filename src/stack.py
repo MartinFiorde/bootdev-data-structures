bde468816adb597cd580d7a5435d6bf16b8217fd
MartinFiorde <fiorde_90@hotmail.com>
2024-10-22 23:10:37 -0300

    2-4 task

--- src/stack.py
+++ src/stack.py
@@ -1,3 +1,12 @@
+# Operation 	Big O 	Description
+# push 	        O(1) 	Add an item to the top of the stack
+# pop 	        O(1) 	Remove and return the top item from the stack
+# peek 	        O(1) 	Return the top item from the stack without modifying the stack
+# size 	        O(1) 	Return the number of items in the stack
+# 
+# Stack operations are limited: no searching, no sorting, no random access
+
+
 class Stack:
     def __init__(self):
         self.items = []
@@ -7,3 +16,9 @@ class Stack:
 
     def size(self):
         return len(self.items)
+
+    def peek(self):
+        pass
+
+    def pop(self):
+        pass
