d9a16016019af2c2c064ce379bef63f6d1281a8c
MartinFiorde <fiorde_90@hotmail.com>
2024-10-31 09:31:51 -0300

    alghoritms 5-4 exponential study

---
+++ src/exponential.py
@@ -0,0 +1,15 @@
+import time
+
+def power_set(input_set):
+    if len(input_set) == 0:
+        return [[]]
+
+    subsets = []
+    first = input_set[0]
+    remaining = input_set[1:]
+    remaining_subsets = power_set(remaining)
+    for subset in remaining_subsets:
+        subsets.append([first] + subset)
+        subsets.append(subset)
+        time.sleep(0.001)
+    return subsets
